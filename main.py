6b74dd74130a1f93d58b8ff8fe2ca5629301bb36
Emon Joy <111835338+EmonJoy@users.noreply.github.com>
2022-09-12 23:36:38 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+import pyautogui
+import time
+
+pyautogui.press("Win")
+pyautogui.write("notepad")
+pyautogui.press("Enter")
+
+time.sleep(1)
+for i in range(10):
+    pyautogui.write("Your device has been hacked!\n")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("firefox")
+pyautogui.press("Enter")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("Chrome")
+pyautogui.press("Enter")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("Calculator")
+pyautogui.press("Enter")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("This pc")
+pyautogui.press("Enter")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("Mircrosoft egde")
+pyautogui.press("Enter")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("camera")
+pyautogui.press("Enter")
+pyautogui.press("Enter")
+
+time.sleep(1)
+
+pyautogui.press("Win")
+pyautogui.write("cmd")
+pyautogui.press("Enter")
+time.sleep(1)
+pyautogui.write("color a")
+pyautogui.press("Enter")
+pyautogui.write("color b")
+pyautogui.press("Enter")
+pyautogui.write("color a")
+pyautogui.press("Enter")
+pyautogui.write("color d")
+pyautogui.press("Enter")
+pyautogui.write("color c")
+pyautogui.press("Enter ")
+pyautogui.write("color a")
+pyautogui.press("Enter")
+time.sleep(1)
+for hack in range(10):
+    pyautogui.write("Hacking....")
+    pyautogui.press("Enter")
+pyautogui.write("*****__System has been successfully hacked by Lucifer Morningstar!!!__*******")
+
+
